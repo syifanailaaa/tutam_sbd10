27f6be7d8cddb7855bd6a0a1f04f2cf260ad6d3c
syifanailaaa <syifanailamaulida@gmail.com>
2026-05-03 22:06:02 +0700

    fix index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(cors({
   origin: [
     "http://localhost:5173",
-    "https://tutamsbd10-production-7cf3.up.railway.app/"
+    "https://tutam-sbd10-two.vercel.app"
   ]
 }))
 app.use(express.json())
